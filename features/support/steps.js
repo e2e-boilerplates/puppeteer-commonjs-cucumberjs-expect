8884dff00cb23d806f6e4a2bbe3ace116346b243
Girma Nigusse <xgirma@gmail.com>
2020-03-08 23:37:44 -0400

    update,update test url

--- features/support/steps.js
+++ features/support/steps.js
@@ -30,7 +30,7 @@ AfterAll(() => {
 
 Given("Navigate to the sandbox", async () => {
   await page
-    .goto("https://e2e-boilerplates.github.io/sandbox/", {
+    .goto("https://e2e-boilerplate.github.io/sandbox/", {
       waitUntil: "networkidle0"
     })
     .catch(() => {});
